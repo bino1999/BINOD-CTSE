ce7e044aef323b174dd66298646bbce1bb6dc810
Binod Bandara <101051395+bino1999@users.noreply.github.com>
2024-04-19 11:25:02 +0530

    for
    check

--- product/route/productRoute.js
+++ product/route/productRoute.js
@@ -7,6 +7,6 @@ router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.put('/:id', productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+// router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
